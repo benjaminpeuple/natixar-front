4ffea6b6d017d1ffbfcc4917c59a3f75084cbd8e
benjaminpeuple <benjamin@bemaos.fr>
2024-05-03 10:04:28 +0200

    fix(auth): added loading time for check authentication

--- src/utils/route-guard/AuthGuard.tsx
+++ src/utils/route-guard/AuthGuard.tsx
@@ -1,5 +1,5 @@
 import { useFusionAuth } from "@fusionauth/react-sdk"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { GuardProps } from "types/auth"
 import { getFusionConfig } from "./FusionConfiguration"
@@ -9,11 +9,20 @@ import { getFusionConfig } from "./FusionConfiguration"
 const AuthGuard = ({ children }: GuardProps) => {
   const { isAuthenticated } = useFusionAuth()
   const { enabled: fusionIsEnabled } = getFusionConfig()
+  const [isLoading, setIsLoading] = useState(true)
   const navigate = useNavigate()
   const location = useLocation()
 
   useEffect(() => {
-    if (fusionIsEnabled && fusionIsEnabled === "true" && !isAuthenticated) {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 500)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  useEffect(() => {
+    if (!isLoading && fusionIsEnabled && fusionIsEnabled === "true" && !isAuthenticated) {
       navigate("authentication/login")
     }
   }, [fusionIsEnabled, isAuthenticated, navigate, location])
